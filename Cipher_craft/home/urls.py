af13435e402dc2df30558cddb47983ceeb98d894
KunalDemla <kunaldemla@gmail.com>
2022-10-28 00:53:04 +0530

    users functionality

---
+++ Cipher_craft/home/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+# from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('',views.index , name='home_page'),
+    path('about/',views.about, name='about_page'),
+]
---
+++ Cipher_craft/users/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('register/', views.register, name='user_register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name='user_login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),name='user_logout'),
+]
