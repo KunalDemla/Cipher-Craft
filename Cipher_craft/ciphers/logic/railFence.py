1b25f7f7b7a5f736c1026a2211bafa9bb873fe8d
KunalDemla <kunaldemla@gmail.com>
2022-11-15 22:46:45 +0530

    user story cards, exception handeling

--- Cipher_craft/ciphers/logic/railFence.py
+++ Cipher_craft/ciphers/logic/railFence.py
@@ -17,36 +17,42 @@ class RailFence:
         return arr
 
     def encrypt(self, plaintext, key):
-        self._plaintext = plaintext.replace(' ', '')
-        self._ciphertext = ""
-        self._key = key
-        L = self._sequence(self._key)
-        temp = L
-        while len(self._plaintext) > len(L):
-            L = L + temp
-        for _ in range(len(L) - len(self._plaintext)):
-            L.pop()
-        num = 0
-        while num < self._key:
-            for i in range(L.count(num)):
-                self._ciphertext = self._ciphertext + self._plaintext[L.index(num)]
-                L[L.index(num)] = self._key
-            num += 1
-        return self._ciphertext
+        try:
+            self._plaintext = plaintext.replace(' ', '')
+            self._ciphertext = ""
+            self._key = key
+            L = self._sequence(self._key)
+            temp = L
+            while len(self._plaintext) > len(L):
+                L = L + temp
+            for _ in range(len(L) - len(self._plaintext)):
+                L.pop()
+            num = 0
+            while num < self._key:
+                for i in range(L.count(num)):
+                    self._ciphertext = self._ciphertext + self._plaintext[L.index(num)]
+                    L[L.index(num)] = self._key
+                num += 1
+            return self._ciphertext
+        except:
+            return "#"
 
     def decrypt(self, ciphertext, key):
-        self._ciphertext = ciphertext
-        self._plaintext = ""
-        self._key = key
-        L = self._sequence(self._key)
-        temp = L
-        while len(self._ciphertext) > len(L):
-            L = L + temp
-        for i in range(len(L) - len(self._ciphertext)):
-            L.pop()
-        temp = sorted(L)
-        for i in L:
-            k = temp.index(i)
-            temp[k] = self._key
-            self._plaintext += self._ciphertext[k]
-        return self._plaintext
+        try:
+            self._ciphertext = ciphertext
+            self._plaintext = ""
+            self._key = key
+            L = self._sequence(self._key)
+            temp = L
+            while len(self._ciphertext) > len(L):
+                L = L + temp
+            for i in range(len(L) - len(self._ciphertext)):
+                L.pop()
+            temp = sorted(L)
+            for i in L:
+                k = temp.index(i)
+                temp[k] = self._key
+                self._plaintext += self._ciphertext[k]
+            return self._plaintext
+        except:
+            return "#"
